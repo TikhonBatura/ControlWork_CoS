fe4fd485b2b0421317ae16229e9c48abf86ab08e
Tikhon <loki_deleoni@live.com>
2023-03-11 20:30:45 +0300

    done task 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] array = new string[4];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            System.Console.WriteLine("Please enter something: ");
+            array[i] = System.Console.ReadLine();
+        }
+        System.Console.WriteLine();
+        System.Console.Write("Entered array: ");
+        PrintArray(array);
+        System.Console.WriteLine();
+        System.Console.Write("Result array: ");
+        PrintArray(showStringLessThree(array));
+
+        void PrintArray(string[] array)
+        {
+            int count = array.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{array[i]}" + " ");
+            }
+            Console.WriteLine();
+        }
+        string[] showStringLessThree(string[] array)
+        {
+            string[] resultArray = new string[array.Length];
+            int length;
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                length = array[i].Length;
+
+                if (length < 4 && length > 0)
+                {
+                    string v = array[i];
+                    resultArray[0] = v;
+                    j++;
+                }
+            }
+            return resultArray;
+        }
+    }
+}
+
